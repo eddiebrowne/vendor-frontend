b9ed7e007ae6fef35ec3a6f88d5ab339da038158
ebrowne <edward@registrationmax.com>
2018-08-09 10:28:25 -0500

    Removed ignored files.

--- vendor-frontend/Pages/receiptService.js
+++
@@ -1,9 +0,0 @@
-var receiptService = function() {
-    var service = {};
-    service.items = new Map();
-    service.total = 0;
-    return service;
-  };
-
-angular.module("vendor-app")
-.factory('receiptService', receiptService);
--- vendor-frontend/vendorController.js
+++
@@ -1,6 +0,0 @@
- var vendorController = function($scope) {
-   $scope.name = "Flower Shop";
- };
-
- angular.module("vendor-app")
- .controller("VendorController", vendorController);
